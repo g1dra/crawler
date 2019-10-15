44836617ccb004d8ac361f2841ede19698ae926f
Darko Vucetic <darko.vucetic7@gmail.com>
2019-10-15 17:08:46 +0200

    Add project files.

---
+++ crawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
