d3a39305f19f09ed726426283d13f6cdc178d002
g1dra <darko@kod.io>
2019-12-24 21:25:46 +0100

    test

--- crawler/Program.cs
+++ crawler/Program.cs
@@ -15,43 +15,9 @@ namespace crawler
         {
             //get page
             HttpClient client = new HttpClient();
-            var response = await client.GetAsync("https://liquipedia.net/dota2/Dota_Pro_Circuit/2018-19/Rankings");
-            var pageContents = await response.Content.ReadAsStringAsync();
-
-            HtmlDocument pageDocument = new HtmlDocument();
-            pageDocument.LoadHtml(pageContents);
-
-            //get team names
-            var nameNodes = pageDocument.DocumentNode.SelectNodes("(//span[contains(@class,'team-template-text')])//a");
-            List<string> teamNamesList = new List<string>();
-            List<float> teamDpcList = new List<float>();
-
-            //add team names to list
-            foreach (var node in nameNodes)
-            {
-                teamNamesList.Add(node.Attributes["title"].Value);
-            }
-
-            //add team dpc point to list
-            var valueNodes = pageDocument.DocumentNode.SelectNodes("(//table[contains(@class,'wikitable')])//td//b");
-            for (int i = 0; i < valueNodes.Count; i++)
-            {
-                if (i%2!=0)
-                {
-
-                    //Console.WriteLine($"tip je : { float.Parse(valueNodes[i].InnerHtml) is float }");
-                    teamDpcList.Add(float.Parse(valueNodes[i].InnerHtml, CultureInfo.InvariantCulture.NumberFormat));
-                }
-            }
-
-            //merge to dictionary
-            var dictionary = teamNamesList.Zip(teamDpcList, (k, v) => new { Key = k, Value = v })
-                .ToDictionary(x => x.Key, x => x.Value);
-
-            foreach (var d in dictionary)
-            {
-                Console.WriteLine($"key je : {d.Key}, value je: {d.Value}");
-            }
+            var result = await client.GetAsync("https://api.opendota.com/api/heroes");
+            var content = result.Content;
+            Console.WriteLine($"Result je {content}");
         }
     }
 }
